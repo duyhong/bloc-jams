c046731ebd321ee9c3cf7c0031655ea542529c21
duyhong <duyhong@berkeley.edu>
2017-03-13 13:17:06 -0700

    checkpoint11-assignment: resubmitted

--- scripts/album.js
+++ scripts/album.js
@@ -79,25 +79,28 @@
          albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
      }
  };
- 
+
+// Creates an array of albums
+var albumArray = [albumPicasso, albumMarconi, albumAlbinoni];
+
+var index = 0; //this variable keeps track of albums in the albumArray
+
  window.onload = function() {
-     setCurrentAlbum(albumAlbinoni);
+     setCurrentAlbum(albumArray[index]);
  };
 
-//var albumCover =
 document.getElementsByClassName("album-cover-art")[0].addEventListener("click", function() {
-    var albumTitle = document.getElementsByClassName('album-view-title')[0];
-/*  console.log(albumTitle);
-    console.log(albumTitle.firstChild.nodeValue);
-   if(albumTitle.firstChild.nodeValue=="The Colors") {
-            setCurrentAlbum(albumMarconi);
-    }
-    else if(albumTitle.firstChild.nodeValue=="The Telephone") {
-            setCurrentAlbum(albumAlbinoni);
+
+    if(index >= albumArray.length - 1) {
+        index = 0;
+        setCurrentAlbum(albumArray[index]);
     }
     else {
-            setCurrentAlbum(albumPicasso);
-    } */
+        index++;
+        setCurrentAlbum(albumArray[index]);
+    }
+/*    var albumTitle = document.getElementsByClassName('album-view-title')[0];
+    for(var i=0; i)
     switch(albumTitle.firstChild.nodeValue) {
         case "The Colors":
             setCurrentAlbum(albumMarconi);
@@ -108,5 +111,6 @@ document.getElementsByClassName("album-cover-art")[0].addEventListener("click", 
         case "Adagio":
             setCurrentAlbum(albumPicasso);
             break;
-    } 
+    }
+*/
 });
